dca147055dae47bfe49d42ce1710a8be8cd3ff58
bc780 <124471008+bc780@users.noreply.github.com>
2024-09-24 15:57:51 -0400

    implemented train and val

---
+++ Resnet18.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C gpu
+#SBATCH -G 1
+#SBATCH -q debug
+#SBATCH -J Resnet18
+#SBATCH --mail-user=bc780@scarletmail.rutgers.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:15:00
+#SBATCH -A m4431_g
+
+#OpenMP settings:
+module load python/3.11
+module load pytorch/2.0.1
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+export MASTER_ADDR=$(hostname)
+export MASTER_PORT=29500
+
+#run the application:
+#applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
+srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=single:1  train.py
--- train.py
+++ train.py
@@ -1,18 +1,69 @@
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
 import torch
-import torchvision.models as models 
+import torchvision.models as models
+import torchvision.datasets as datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import logging
+import torch.nn as nn
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
+batchsize = 64
+epochs=5
+lr = 0.001
 
-resnet = models.resnet18()
+def train(device, model, dataLoader, loss_fn):
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    sum = 0.0
+    count = 0.0
+    for batch, (image, label) in enumerate(dataLoader):
+        image = image.to(device)
+        label = label.to(device)
+
+        pred = model(image)
+        loss = loss_fn(pred, label)
+        
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(image)
+            sum += (current/len(dataLoader.dataset))
+            count += 1.0
+    return sum/count
+
+def val(device, model, dataLoader):
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in dataLoader:
+            image = image.to(device)
+            label = label.to(device)
+
+            pred = model(image)
+            correct += (pred.argmax(1) == label).type(torch.float).sum().item()
+    return correct/len(dataLoader.dataset)
+
+
+if __name__ == "__main__":
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
+    train_data = datasets.CIFAR10(train=True, transform=ToTensor())
+    val_data = datasets.CIFAR10(train=False, transform=ToTensor())
+
+    train_dataloader = DataLoader(train_data, batch_size=batchsize, shuffle=True)
+    val_dataloader = DataLoader(val_data, batch_size=batchsize, shuffle=True)
+
+    model = models.resnet18()
+    loss_fn = nn.CrossEntropyLoss()
+
+    for i in range(epochs):
+        logging.info("Epoch %i", i+1)
+        train_loss = train(device, model, train_dataloader, loss_fn)
+        logging.info("Training loss: %f", train_loss)
+        val_loss = val(device, model, val_dataloader)
+        logging.info("Validation loss: %f", val_loss)
